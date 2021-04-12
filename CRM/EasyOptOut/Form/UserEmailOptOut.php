1c2a3de9132f8283a3c1e02e4c24efc3622b05c3
akosgarai <szlem01@gmail.com>
2021-04-12 17:31:29 +0200

    Cleanup form

--- CRM/EasyOptOut/Form/UserEmailOptOut.php
+++ CRM/EasyOptOut/Form/UserEmailOptOut.php
@@ -9,74 +9,38 @@ use CRM_EasyOptOut_ExtensionUtil as E;
  */
 class CRM_EasyOptOut_Form_UserEmailOptOut extends CRM_Core_Form
 {
+    /**
+     * Preprocess form
+     *
+     * @throws CRM_Core_Exception
+     */
+    public function preProcess()
+    {
+        // The auto submit process will be implemented with js, so that i will need to
+        // some custom js files here.
+        parent::preProcess();
+    }
+
     public function buildQuickForm()
     {
-
-    // add form elements
-        $this->add(
-            'select', // field type
-      'favorite_color', // field name
-      'Favorite Color', // field label
-      $this->getColorOptions(), // list of options
-      true // is required
-        );
-        $this->addButtons(array(
-      array(
-        'type' => 'submit',
-        'name' => E::ts('Submit'),
-        'isDefault' => true,
-      ),
-    ));
-
-        // export form elements
-        $this->assign('elementNames', $this->getRenderableElementNames());
+        // Only hidden inputs supposed to be added here, as it will be submitted with the js.
         parent::buildQuickForm();
+    }
+
+    /**
+     * Set default values
+     *
+     * @return array
+     */
+    public function setDefaultValues()
+    {
+        // set the value of the hidden inputs
+        return $this->_defaults;
     }
 
     public function postProcess()
     {
-        $values = $this->exportValues();
-        $options = $this->getColorOptions();
-        CRM_Core_Session::setStatus(E::ts('You picked color "%1"', array(
-      1 => $options[$values['favorite_color']],
-    )));
+        // apply user opt out
         parent::postProcess();
-    }
-
-    public function getColorOptions()
-    {
-        $options = array(
-      '' => E::ts('- select -'),
-      '#f00' => E::ts('Red'),
-      '#0f0' => E::ts('Green'),
-      '#00f' => E::ts('Blue'),
-      '#f0f' => E::ts('Purple'),
-    );
-        foreach (array('1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e') as $f) {
-            $options["#{$f}{$f}{$f}"] = E::ts('Grey (%1)', array(1 => $f));
-        }
-        return $options;
-    }
-
-    /**
-     * Get the fields/elements defined in this form.
-     *
-     * @return array (string)
-     */
-    public function getRenderableElementNames()
-    {
-        // The _elements list includes some items which should not be
-        // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
-        // items don't have labels.  We'll identify renderable by filtering on
-        // the 'label'.
-        $elementNames = array();
-        foreach ($this->_elements as $element) {
-            /** @var HTML_QuickForm_Element $element */
-            $label = $element->getLabel();
-            if (!empty($label)) {
-                $elementNames[] = $element->getName();
-            }
-        }
-        return $elementNames;
     }
 }
